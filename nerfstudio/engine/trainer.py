a32429c968a0af633f518aa8a38f68ff4c159ccf
himalaya <hyperzy.yang@gmail.com>
2023-07-03 06:31:11 -0400

    - Update pyproject.tomal to accomodate the env on PACE

--- nerfstudio/engine/trainer.py
+++ nerfstudio/engine/trainer.py
@@ -177,14 +177,15 @@ class Trainer:
                     self.save_checkpoint(step)
 
                 writer.write_out_storage()
-            # save checkpoint at the end of training
-            self.save_checkpoint(step)
+        # save checkpoint at the end of training
+        self.save_checkpoint(step)
+        writer.write_out_storage()
 
-            CONSOLE.rule()
-            CONSOLE.print("[bold green]:tada: :tada: :tada: Training Finished :tada: :tada: :tada:", justify="center")
-            if not self.config.viewer.quit_on_train_completion:
-                CONSOLE.print("Use ctrl+c to quit", justify="center")
-                self._always_render(step)
+        CONSOLE.rule()
+        CONSOLE.print("[bold green]:tada: :tada: :tada: Training Finished :tada: :tada: :tada:", justify="center")
+        if not self.config.viewer.quit_on_train_completion:
+            CONSOLE.print("Use ctrl+c to quit", justify="center")
+            self._always_render(step)
 
     @check_main_thread
     def _always_render(self, step):
